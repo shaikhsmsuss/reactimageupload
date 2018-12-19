24635831f3fb2353efc0a0877bf153dbca43758c
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2018-12-20 00:52:07 +0530

    main rendering component

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Main from './components/Main';
+import './App.css';
+
+const App=()=>(
+  <div>
+    <h1>
+      file upload
+    </h1>
+    <Main/>
+  </div>
+)
+
+export default App;
